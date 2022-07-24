868089c6803d9f7a0cd8116d6a7d4fc486760add
eric jubera <ericjubera@gmail.com>
2022-07-24 16:44:12 -0500

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2=document.createElement("h2");
-h2.textContent="This content added ny JavaScript";
+h2.textContent="This content added my JavaScript";
 document.querySelector("body").appendChild(h2)
